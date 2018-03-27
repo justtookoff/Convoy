5bec0fc55b6a7ac40a25cbed7df9e4dce7bd9ce5
Dan <dhkwon7740@gmail.com>
2018-03-27 06:16:31 -0700

    Calculate Distance

--- convoy.py
+++ convoy.py
@@ -11,17 +11,23 @@ def main():
 	cap = cv2.VideoCapture(0)
 	
 	# To save Video	
-	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	#fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	#out1 = cv2.VideoWriter('output1.avi', fourcc, 20.0, (640, 480))
-	out2 = cv2.VideoWriter('output2.avi', fourcc, 20.0, (640, 480))
+	#out2 = cv2.VideoWriter('output2.avi', fourcc, 20.0, (640, 480))
+
+	#Print Width and Height
+	if cap.isOpened():
+		width = cap.get(3)
+		height = cap.get(4)
+		followingCar = (width/2, height)
+		print(width)
+		print(height)
 
 	while True:
     	# Capture frame-by-frame
 		ret, frame = cap.read()
 		
-		if ret == True:
-			#frame = cv2.flip(frame,1)
-	
+		if ret == True:	
 	    	# converts images from BGR to HSV
 			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 		
@@ -34,33 +40,55 @@ def main():
 			mask = cv2.erode(mask, None, iterations=2)
 			mask = cv2.dilate(mask, None, iterations=2)
 			
-			#mask = cv2.flip(mask,1)
 			# Bitwise-AND mask and original image
 			#res = cv2.bitwise_and(frame,frame, mask= mask)
 		
-			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, 
+				cv2.CHAIN_APPROX_SIMPLE)[-2]
 			center = None
-		
+			
 			if len(cnts) > 0:
 				c = max(cnts, key=cv2.contourArea)
 				(x,y,w,h) = cv2.boundingRect(c)
 				center = (x+w/2, y+h/2)
+				
+				#Calculate the distance with Y coordinate AND Show the distance on the screen
+				halfWidth = width / 2
+				distanceY = height - (y+h/2)
+				distanceX = halfWidth - (x+w/2) 
+				direction = None
+				
+				#direction = 1 means right
+				#direction = 0 means straight
+				#direction = -1 means left 
+				if(distanceX < 0):
+					direction = 1
+				elif(distanceX == 0):
+					direction = 0
+				else:
+					direction = -1
+
+				cv2.putText(frame, "disX: {}, disY: {}, direction: {}".format(distanceX, distanceY, direction), 
+					(10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+				
 
 				if w > 10 and h> 10:
-					#	print(x-225)
-					cv2.rectangle(frame, (int(x), int(y)), (x+w, y+h), (0,255, 0), 2)
-					cv2.line(frame, (600/2, 0), (600/2, 480), (0, 255, 0), 2)
+					cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255, 0), 2)
+					
+					#Showing Rectangle contour line and Middle line
+					#cv2.line(frame, (600/2, 0), (600/2, 480), (0, 255, 0), 2)
+					#cv2.line(frame, center, (600/2, y+h/2), (0,255,0), 2)
+					
 
-					#cv2.circle(frame, center, 1, (0,0, 255), -1)
-					cv2.line(frame, center, (600/2, y+h/2), (0,255,0), 2)
+					cv2.rectangle(mask, (x, y), (x+w, y+h), (0, 255, 0), 2)
 					
-					cv2.rectangle(mask, (int(x), int(y)), (x+w, y+h), (0, 255, 0), 2)
-					cv2.line(mask, (600/2, 0), (600/2, 480), (0, 255, 0), 2)
-					
-					cv2.line(mask, center, (600/2, y+h/2), (0,255, 0), 2)
-		
+					#Whowing Rectangel contour line and Middle line
+					#cv2.line(mask, (600/2, 0), (600/2, 480), (0, 255, 0), 2)
+					#cv2.line(mask, center, (600/2, y+h/2), (0,255, 0), 2)
+			
+			#To write on the video
 			#out1.write(frame)
-			out2.write(mask)
+			#out2.write(mask)
 
 			cv2.imshow('frame',frame)
 			cv2.imshow('mask',mask)
@@ -72,10 +100,11 @@ def main():
 
 		else:
 			break
+
 	# When everything done, release the capture
 	cap.release()
 	#out1.release()
-	out2.release()
+	#out2.release()
 	cv2.destroyAllWindows()
 
 if __name__=='__main__':
